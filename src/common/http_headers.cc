11731d9e16c03f1c6408fe1c0126e2a207c85229
TomerJLevy <TomerJlevy@gmail.com>
2019-12-19 21:23:07 +0200

    Fix compilation error
    std::equal takes 3 iterators or 3 iterators and a predicate function

--- src/common/http_headers.cc
+++ src/common/http_headers.cc
@@ -43,7 +43,7 @@ toLowercase(std::string str) {
 
 bool
 LowercaseEqualStatic(const std::string& dynamic, const std::string& statik) {
-    return std::equal(dynamic.begin(), dynamic.end(), statik.begin(), statik.end(),
+    return std::equal(dynamic.begin(), dynamic.end(), statik.begin(),
         [] (const char& a, const char& b) {
             return std::tolower(a) == b;
         });
